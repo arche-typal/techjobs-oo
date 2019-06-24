12e30e2d4e4197998ceeaf0fe66d9c81f9af3a89
Archetypal <archetypal.0@gmail.com>
2019-06-24 17:02:20 -0500

    Completed TODOS.

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,7 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
+import org.launchcode.models.JobField;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -20,8 +22,11 @@ public class JobController {
     private JobData jobData = JobData.getInstance();
 
     // The detail display for a given Job at URLs like /job?id=17
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(Model model, int id) {
+    @RequestMapping(value = "", method = RequestMethod.GET) ///job?id=X //@RequestMapping(value = "edit/{cheeseId}", method = RequestMethod.GET)
+    public String index(Model model, int id) { //grabs id from html file
+        model.addAttribute("job", jobData.findById(id));
+        //Job newJob = jobData.findById(id);
+
 
         // TODO #1 - get the Job with the given ID and pass it into the view
 
@@ -40,8 +45,41 @@ public class JobController {
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
+        int jobId = 0;
 
-        return "";
+             if (errors.hasErrors()) {
+             return "new-job";
+             }
+
+         //if valid create a new Job and add it to the jobData data store.
+
+         //create a new Job
+         //instantiate/initialize
+         // Job(String aName, Employer aEmployer, Location aLocation,
+         //               PositionType aPositionType, CoreCompetency aSkill)
+         //may use IDs
+
+         //Job newJob = new Job(jobForm.getName(),jobForm.getEmployerId(),jobForm.getLocationId(), jobForm.getPositionTypeId(), jobForm.getCoreCompetencyId());
+        Job newJob = new Job(jobForm.getName(), jobData.getEmployers().findById(jobForm.getEmployerId()), jobData.getLocations().findById(jobForm.getLocationId()), jobData.getPositionTypes().findById(jobForm.getPositionTypeId()), jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId()));
+        //Create the new Job.^ If you don't write 'new' it will overlay the current instance of a Job object.
+
+        //Apple the getId method to the newJob object.
+        //jobId = newJob.getId();
+
+         //JobData data = new JobData();
+
+         //JobData.getInstance().add(newJob);
+        jobData.add(newJob); //HELL FUCKIN' YEAH!
+        //return "job?id={newJob.getId()}"; //return "redirect:";
+        //System.out.println("Print line statement");
+         //return "job-detail"; //return "redirect:";
+        return "redirect:/job/?id=" + newJob.getId();
+        //Here you are using redirect: to go to a webpage.
+
+
+        //return ""; //remember to comment out.
+
+
 
     }
 }
--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -28,6 +28,18 @@ public class JobForm {
         Don't forget to add getters and setters
      */
 
+    @NotNull
+    private int locationId;
+
+    @NotNull
+    private int coreCompetencyId;
+
+    @NotNull
+    private int positionTypeId;
+
+    @NotNull
+    private int id;
+
     private ArrayList<Employer> employers;
     private ArrayList<Location> locations;
     private ArrayList<CoreCompetency> coreCompetencies;
@@ -42,6 +54,9 @@ public class JobForm {
         */
 
         employers = jobData.getEmployers().findAll();
+        locations = jobData.getLocations().findAll();
+        coreCompetencies = jobData.getCoreCompetencies().findAll();
+        positionTypes = jobData.getPositionTypes().findAll();
 
     }
 
@@ -59,6 +74,30 @@ public class JobForm {
 
     public void setEmployerId(int employerId) {
         this.employerId = employerId;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public int getCoreCompetencyId() {
+        return coreCompetencyId;
+    }
+
+    public void setCoreCompetencyId(int coreCompetencyId) {
+        this.coreCompetencyId = coreCompetencyId;
+    }
+
+    public int getPositionTypeId() {
+        return positionTypeId;
+    }
+
+    public void setPositionTypeId(int positionTypeId) {
+        this.positionTypeId = positionTypeId;
     }
 
     public ArrayList<Employer> getEmployers() {
@@ -91,5 +130,13 @@ public class JobForm {
 
     public void setPositionTypes(ArrayList<PositionType> positionTypes) {
         this.positionTypes = positionTypes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
